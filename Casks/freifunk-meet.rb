28e6d79ba3f218bb60f4410c24d71eb1f1eceea1
Simon <simphide@users.noreply.github.com>
2021-05-07 20:04:12 +0200

    Bump to 2.1.1

--- Casks/freifunk-meet.rb
+++ Casks/freifunk-meet.rb
@@ -1,6 +1,6 @@
 cask "freifunk-meet" do
-  version "2.1.0"
-  sha256 "6176edfe3d43036d41c2aecf2418f2344777cbf9f296d02db037442eb5c74180"
+  version "2.1.1"
+  sha256 "9397c5e37f8b2c67872b79dd0bfa80c59f85b875fcb77f6f18b8ddc36bc1c46e"
 
   url "https://github.com/freifunkMUC/jitsi-meet-electron/releases/download/v#{version}/ffmuc-meet.dmg"
   appcast "https://github.com/FreifunkMUC/jitsi-meet-electron/releases.atom"
